cee0cb4b083089eaa9d250865beb15bc2da56e65
qwer0114 <rnalgh0114@gmail.com>
2025-01-18 20:15:43 +0900

    ✨ feat(custom): 선택한 전형의 카테고리를 선택하는 시나리오 정의

---
+++ src/components/chat-step/choose-admission-category/choose-admission-category.tsx
@@ -0,0 +1,44 @@
+import AdmissionCategoryMenus from '@/components/chat-step/choose-admission-category/admission-category-menus';
+import Chat from '@/components/chat/chat';
+import systemMessage from '@/constants/message';
+import useAdmissionDetail from '@/hooks/querys/useAdmissionDetail';
+import useAdmissionStore from '@/stores/store/admission-store';
+import useMessagesStore from '@/stores/store/message-store';
+import { ADDMISSION } from '@/types/admission-type';
+import { ChatSteps } from '@/types/chat';
+
+interface Props {
+  changeStep: (step: ChatSteps) => void;
+}
+
+function ChooseAdmissionCategory({ changeStep }: Props) {
+  const { setMessages } = useMessagesStore();
+  const { admissionType, setAdmissionCategory } = useAdmissionStore();
+  const data = useAdmissionDetail(admissionType);
+
+  const selectCategory = (catgory: string) => {
+    changeStep('choose_detail');
+    setMessages([
+      {
+        role: 'system',
+        message: systemMessage.admissionGuide(ADDMISSION[admissionType!]),
+      },
+      {
+        role: 'user',
+        message: catgory,
+      },
+    ]);
+    setAdmissionCategory(catgory);
+  };
+
+  return (
+    <div>
+      <Chat role="system">{systemMessage.admissionGuide(ADDMISSION[admissionType!])}</Chat>
+      <div className="mt-2">
+        <AdmissionCategoryMenus admissionType={admissionType!} data={data} selectCategory={selectCategory} />
+      </div>
+    </div>
+  );
+}
+
+export default ChooseAdmissionCategory;
