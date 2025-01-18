22072e71fa578b4850677f2e853dbbcd86f79381
qwer0114 <rnalgh0114@gmail.com>
2025-01-18 20:10:15 +0900

    💄 style(custom): 배경화면 흰색 추가

--- src/components/flyout/trigger.tsx
+++ src/components/flyout/trigger.tsx
@@ -10,7 +10,13 @@ function Trigger({ children, className }: TirggerProps) {
   const { isOpen, setIsOpen } = useFlyoutContext();
   return (
     <div onMouseLeave={() => setIsOpen(false)} onMouseOver={() => setIsOpen(true)}>
-      <div className={cn('p-2', isOpen ? 'bg-primary text-white' : 'hover:bg-primary hover:text-white', className)}>
+      <div
+        className={cn(
+          'bg-white p-2',
+          isOpen ? 'bg-primary text-white' : 'hover:bg-primary hover:text-white',
+          className,
+        )}
+      >
         {children}
         <div
           className={cn('bg-image-tree-right absolute inset-0', isOpen ? 'opacity-100' : 'opacity-0 hover:opacity-100')}
