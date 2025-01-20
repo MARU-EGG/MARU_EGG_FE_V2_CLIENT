823e81a5f8447bc9db4316687bf8a948914002cb
qwer0114 <rnalgh0114@gmail.com>
2025-01-20 19:15:11 +0900

    ✨ feat(custom): 각 스텝 Funnel 컴포넌트로 정의

--- src/page/components/main/main.tsx
+++ src/page/components/main/main.tsx
@@ -1,51 +1,54 @@
 import { Suspense, useState } from 'react';
-import Markdown from 'react-markdown';
-import Chat from '@/components/chat/chat';
 import AdmissionCategoryResult from '@/page/components/chat-step/admission-category-result/admission-category-result';
 import ChooseAdmissionCategory from '@/page/components/chat-step/choose-admission-category/choose-admission-category';
 import ChooseAdmission from '@/page/components/chat-step/choose-admission/choose-admission';
 import QuestionResult from '@/page/components/chat-step/question-result/question-result';
-import useMessagesStore from '@/stores/store/message-store';
+import ReferenceResult from '@/page/components/chat-step/reference-result/reference-result';
+import Funnel from '@/page/components/funnel/funnel';
+import MessageHistory from '@/page/components/message-history/message-history';
+import useAdmissionStore from '@/stores/store/admission-store';
 import { ChatSteps } from '@/types/chat';
 
 function Main() {
-  const [steps, setSteps] = useState<ChatSteps>('choose_admission');
-  const { messages } = useMessagesStore();
+  const [steps, setSteps] = useState<ChatSteps>('입시유형 선택');
+  const { admissionType, admissionCategory, question } = useAdmissionStore();
 
   const changeStep = (step: ChatSteps) => {
     setSteps(step);
   };
 
   return (
-    <main className="flex h-full flex-col gap-6 overflow-y-auto py-6 pl-12 pr-3">
-      <div>
-        <div className="flex flex-col gap-2">
-          {messages?.map((message, index) =>
-            message.markdown ? (
-              <Chat key={index} role={message.role}>
-                <Markdown>{message.message}</Markdown>
-              </Chat>
-            ) : (
-              <Chat key={index} role={message.role}>
-                {message.message}
-              </Chat>
-            ),
-          )}
-        </div>
-      </div>
-      <div>
-        {steps === 'choose_admission' ? (
+    <main className="flex h-full flex-col gap-6 overflow-y-auto overflow-x-hidden py-6 pl-12 pr-3">
+      <MessageHistory />
+
+      <Funnel step={steps}>
+        <Funnel.Step step="입시유형 선택">
           <ChooseAdmission changeStep={changeStep} />
-        ) : steps === 'choose_admission_category' ? (
+        </Funnel.Step>
+        <Funnel.Step step="입시유형 상세전형 선택">
           <Suspense>
-            <ChooseAdmissionCategory changeStep={changeStep} />
+            <ChooseAdmissionCategory changeStep={changeStep} admissionType={admissionType!} />
           </Suspense>
-        ) : steps === 'admission_category_result' ? (
-          <AdmissionCategoryResult changeStep={changeStep} />
-        ) : (
-          <QuestionResult />
-        )}
-      </div>
+        </Funnel.Step>
+        <Funnel.Step step="상세전형 선택 결과">
+          <AdmissionCategoryResult
+            changeStep={changeStep}
+            admissionType={admissionType!}
+            admissionCategory={admissionCategory!}
+          />
+        </Funnel.Step>
+        <Funnel.Step step="상세전형 질문 결과">
+          <QuestionResult
+            admissionType={admissionType!}
+            admissionCategory={admissionCategory!}
+            question={question!}
+            changeStep={changeStep}
+          />
+        </Funnel.Step>
+        <Funnel.Step step="질문 출처 결과">
+          <ReferenceResult changeStep={changeStep} />
+        </Funnel.Step>
+      </Funnel>
     </main>
   );
 }
