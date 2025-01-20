978578e1aac4132bb32212578c6ae1b8e7f64527
qwer0114 <rnalgh0114@gmail.com>
2025-01-20 19:19:56 +0900

    ✨ feat(custom): 입학 전형 카테고리 선택 결과 컴포넌트 구현

--- src/page/components/chat-step/admission-category-result/admission-category-result.tsx
+++ src/page/components/chat-step/admission-category-result/admission-category-result.tsx
@@ -1,37 +1,39 @@
-import ResultMessage from '@/components/chat-step/admission-category-result/result-message';
 import PresetButton from '@/components/preset-button/preset-button';
-import PRESET_BUTTON from '@/constants/preset-buttons';
+import PRESET_BUTTON, { AdmissionPresetType } from '@/constants/preset-buttons';
 import useAdmissionQuestionResult from '@/hooks/use-admission-question-result';
 import useAdmissionStore from '@/stores/store/admission-store';
 import useMessagesStore from '@/stores/store/message-store';
+import { AdmissionType } from '@/types/admission-type';
 import { ChatSteps } from '@/types/chat';
 
 interface Props {
   changeStep: (step: ChatSteps) => void;
+  admissionType: AdmissionType;
+  admissionCategory: string;
 }
 
-function AdmissionCategoryResult({ changeStep }: Props) {
-  const { admissionType, admissionCategory, setQuestion } = useAdmissionStore();
-  const { result, isLoading } = useAdmissionQuestionResult(admissionType!, admissionCategory!);
+function AdmissionCategoryResult({ admissionType, changeStep, admissionCategory }: Props) {
+  const { setQuestion } = useAdmissionStore();
+  const { result, isLoading } = useAdmissionQuestionResult({
+    admissionType: admissionType,
+    category: 'ADMISSION_GUIDELINE',
+    question: admissionCategory,
+    questionType: 'general',
+  });
   const { setMessages } = useMessagesStore();
 
-  const selectQuestion = (question: { label: string; category: string }) => {
-    if (result) {
-      changeStep('question_result');
-      setMessages([
-        { role: 'system', message: result.answer.content, markdown: true },
-        { role: 'user', message: question.label },
-      ]);
-      setQuestion(question);
-    }
+  const selectQuestion = (question: AdmissionPresetType) => {
+    changeStep('상세전형 질문 결과');
+    setMessages([{ role: 'user', message: question.label }]);
+    setQuestion(question);
   };
 
   if (!isLoading && result)
     return (
       <div>
-        <ResultMessage result={result} />
         <div className="mt-2 flex w-full justify-end">
-          <div className="flex w-56 flex-wrap justify-end gap-2">
+          <div className="flex w-72 flex-wrap justify-end gap-2">
+            <PresetButton onClick={() => changeStep('질문 출처 결과')}>🙋‍♂️ 어디에서 볼 수 있나요?</PresetButton>
             {PRESET_BUTTON.map((question) => (
               <PresetButton
                 onClick={() => {
@@ -42,6 +44,7 @@ function AdmissionCategoryResult({ changeStep }: Props) {
                 {question.label}
               </PresetButton>
             ))}
+            <PresetButton>조건 재설정</PresetButton>
           </div>
         </div>
       </div>
