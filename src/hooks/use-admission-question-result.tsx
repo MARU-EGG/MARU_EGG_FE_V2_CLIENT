c9d7076b337344c8051a7a38ce534fbac2bc0df0
qwer0114 <rnalgh0114@gmail.com>
2025-01-20 19:12:32 +0900

    ✨ feat(custom): 입학 질문에 대한 응답을 반환하는 커스텀 훅 정의

--- src/hooks/use-admission-question-result.tsx
+++ src/hooks/use-admission-question-result.tsx
@@ -1,29 +1,85 @@
 import { useEffect, useState } from 'react';
+import systemMessage from '@/constants/message';
 import usePostQuestion from '@/hooks/querys/usePostQuestion';
+import useMessagesStore from '@/stores/store/message-store';
+import useQuestionReferencesStore from '@/stores/store/question-references-store';
 import { AdmissionType } from '@/types/admission-type';
 import { PostQuestionResponse } from '@/types/questions';
 import makePrompt from '@/utils/make-prompt';
+import { useQueryClient } from '@tanstack/react-query';
 
-export default function useAdmissionQuestionResult(admissionType: AdmissionType, admissionCategory: string) {
+interface UseAdmissionType {
+  admissionType: AdmissionType;
+  category: 'ADMISSION_GUIDELINE' | 'PASSING_RESULT';
+  question: string;
+  questionType: 'general' | 'detail';
+}
+
+export default function useAdmissionQuestionResult({
+  admissionType,
+  category,
+  question,
+  questionType,
+}: UseAdmissionType) {
   const [result, setResult] = useState<PostQuestionResponse | null>(null);
+  const [timestamp, setTimestamp] = useState(Date.now());
   const { questionMutation } = usePostQuestion();
+  const { setMessages } = useMessagesStore();
+  const { setReferences } = useQuestionReferencesStore();
+  const queryClient = useQueryClient();
+
+  const checkAndSetCacheData = (QUERY_KEY: unknown[]) => {
+    const cachedData = queryClient.getQueryData<string>(QUERY_KEY);
+    if (cachedData) {
+      setMessages([
+        {
+          role: 'system',
+          message: cachedData,
+          markdown: true,
+        },
+      ]);
+      return true;
+    }
+    return false;
+  };
 
   useEffect(() => {
-    if (admissionType && admissionCategory) {
-      questionMutation.mutate(
-        {
-          type: admissionType,
-          category: 'ADMISSION_GUIDELINE',
-          content: makePrompt(admissionType, admissionCategory),
-        },
-        {
-          onSuccess: (data) => {
-            setResult(data);
-          },
-        },
-      );
-    }
-  }, []);
+    if (!admissionType || !question) return;
 
-  return { result, isLoading: questionMutation.isPending };
+    const QUERY_KEY = [admissionType, category, question, questionType];
+    if (checkAndSetCacheData(QUERY_KEY)) return;
+
+    const content = questionType === 'general' ? makePrompt(admissionType, question) : question;
+
+    questionMutation.mutate(
+      {
+        type: admissionType,
+        category: category,
+        content: content,
+      },
+      {
+        onSuccess: (data) => {
+          setResult(data);
+          setMessages([
+            {
+              role: 'system',
+              message: data.answer.content,
+              markdown: true,
+            },
+          ]);
+          setReferences(data.references);
+          queryClient.setQueryData(QUERY_KEY, data.answer.content);
+        },
+        onError: () => {
+          setMessages([{ role: 'system', message: systemMessage.errorMessage }]);
+        },
+      },
+    );
+  }, [question, timestamp]);
+
+  const refetch = () => {
+    setTimestamp(Date.now());
+  };
+
+  return { result, isLoading: questionMutation.isPending, refetch };
 }
