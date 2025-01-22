845286656bf53e9d05ae6f26c72fcf67d31a705b
qwer0114 <rnalgh0114@gmail.com>
2025-01-22 19:53:07 +0900

    📦 chore(custom): input send 이미지 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,3 +2,4 @@ export { default as ArrowDownIcon } from '@assets/svg/arrow-down.svg?react';
 export { default as TreeIcon } from '@assets/svg/tree.svg?react';
 export { default as ReloadIcon } from '@assets/svg/reload.svg?react';
 export { default as MaruIcon } from '@assets/svg/maru.svg?react';
+export { default as SendIcon } from '@assets/svg/send.svg?react';
