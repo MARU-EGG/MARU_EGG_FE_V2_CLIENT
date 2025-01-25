d2cd7fe7e61cc4df4c21dc21c46dbd7eaba70020
qwer0114 <rnalgh0114@gmail.com>
2025-01-25 19:20:27 +0900

    ♻️ refactor(custom): 기존 Selector 컴포넌트 MenuList로 이름 변경

--- src/components/selector/select-header.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function SelectorHeader({ children }: { children: React.ReactNode }) {
-  return <div className="bg-image-tree-right rounded-t-lg bg-primary p-4 text-title text-white">{children}</div>;
-}
-
-export default SelectorHeader;
