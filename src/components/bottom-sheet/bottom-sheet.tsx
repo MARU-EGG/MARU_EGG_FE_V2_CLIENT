3f5a78fd4b408949ebaf81a862977f7c53ca8813
qwer0114 <rnalgh0114@gmail.com>
2025-01-30 14:18:40 +0900

    💄 style(custom): 애니메이션 제거

--- src/components/bottom-sheet/bottom-sheet.tsx
+++ src/components/bottom-sheet/bottom-sheet.tsx
@@ -15,11 +15,11 @@ function BottomSheet({ open, onClose, children }: BottomSheetProps) {
     <div
       onTransitionEnd={onTransitionEnd}
       onClick={onClose}
-      className={`animate-fadein absolute inset-0 z-30 bg-black/60 transition-opacity duration-300 ${animationTrigger ? 'opacity-100' : 'opacity-0'}`}
+      className={`absolute inset-0 z-30 bg-black/60 transition-opacity duration-300 ${animationTrigger ? 'opacity-100' : 'opacity-0'}`}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`animate-slideUp absolute bottom-0 z-40 h-80 w-full overflow-y-auto rounded-t-2xl bg-white px-4 py-6 transition-transform duration-300 ${animationTrigger ? 'translate-y-0' : 'translate-y-full'}`}
+        className={`absolute bottom-0 z-40 h-80 w-full overflow-y-auto rounded-t-2xl bg-white px-4 py-6 transition-transform duration-300 ${animationTrigger ? 'translate-y-0' : 'translate-y-full'}`}
       >
         {children}
       </div>
