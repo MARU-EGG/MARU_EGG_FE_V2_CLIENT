38559624d7db21cb0800907acbc2a59bdeeef48e
qwer0114 <rnalgh0114@gmail.com>
2025-02-03 16:44:10 +0900

    💄 style(custom): tailwind lint 수정

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,7 @@
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-dvh items-center justify-center bg-[#EEEE]">
-      <div className="mobile:h-full mobile:min-h-[480px] mobile:min-w-[320px] mobile:rounded-none desktop:h-[780px] desktop:max-w-[390px] desktop:rounded-3xl desktop:border desktop:border-gray-200 desktop:shadow-2xl relative flex h-full w-full flex-col overflow-hidden border bg-[#E7E7E7]">
+      <div className="relative flex h-full w-full flex-col overflow-hidden border bg-[#E7E7E7] mobile:h-full mobile:min-h-[480px] mobile:min-w-[320px] mobile:rounded-none desktop:h-[780px] desktop:max-w-[390px] desktop:rounded-3xl desktop:border desktop:border-gray-200 desktop:shadow-2xl">
         {children}
       </div>
     </div>
