f50a098814f434b3263568d91946a0fbb456b34f
qwer0114 <rnalgh0114@gmail.com>
2025-02-04 19:59:36 +0900

    ♻️ refactor(custom): 컴포넌트 가독성을 높이기 위해 내부 컴포넌트로 분리

--- src/components/header/contact.tsx
+++ src/components/header/contact.tsx
@@ -1,32 +1,31 @@
+interface ContactItemProps {
+  title: string;
+  department: string;
+  numbers: number[];
+}
+function ContactItem({ title, department, numbers }: ContactItemProps) {
+  return (
+    <div className="flex text-label text-gray-500">
+      <span className="w-1/2">{title}</span>
+      <span className="flex-1 whitespace-nowrap">
+        {department} :{' '}
+        <a href={`tel:02-300-${numbers[0]}`} className="underline underline-offset-4">
+          02)300-{numbers[0]}
+        </a>
+        ,{' '}
+        <a href={`tel:02-300-${numbers[1]}`} className="underline underline-offset-4">
+          {numbers[1]}
+        </a>
+      </span>
+    </div>
+  );
+}
+
 function Contact() {
   return (
     <div className="flex flex-col gap-3">
-      <div className="flex text-label text-gray-500">
-        <span className="w-1/2">학생부교과, 실기/실적 위주 문의</span>
-        <span className="flex-1 whitespace-nowrap">
-          입학관리팀 :{' '}
-          <a href="tel:02-300-1799" className="underline underline-offset-4">
-            02)300-1799
-          </a>
-          ,{' '}
-          <a href="tel:02-300-1800" className="underline underline-offset-4">
-            1800
-          </a>
-        </span>
-      </div>
-      <div className="flex text-label text-gray-500">
-        <span className="w-1/2">학생부종합 문의</span>
-        <span className="flex-1 whitespace-nowrap">
-          인재발굴팀 :{' '}
-          <a href="tel:02-300-1794" className="underline underline-offset-4">
-            02)300-1794
-          </a>
-          ,{' '}
-          <a href="tel:02-300-1844" className="underline underline-offset-4">
-            1844
-          </a>
-        </span>
-      </div>
+      <ContactItem title="학생부교과, 실기/실적 위주 문의" department="입학관리팀" numbers={[1799, 1800]} />
+      <ContactItem title="학생부종합 문의" department="인재발굴팀" numbers={[1794, 1844]} />
     </div>
   );
 }
