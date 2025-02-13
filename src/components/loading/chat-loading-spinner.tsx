dbe7fc4911db100d6deb553859b74c3124631c65
qwer0114 <rnalgh0114@gmail.com>
2025-02-13 15:25:13 +0900

    ✨ feat(custom): 채팅 로딩 스피너 컴포넌트 정의

---
+++ src/components/loading/chat-loading-spinner.tsx
@@ -0,0 +1,13 @@
+import ChatLoadingAnimation from '@/assets/lottie/chat-loading.json';
+import Chat from '@/components/chat/chat';
+import Lottie from 'lottie-react';
+
+function ChatLoadingSpinner() {
+  return (
+    <Chat role="system">
+      <Lottie className="h-28" animationData={ChatLoadingAnimation} data="채팅"></Lottie>
+    </Chat>
+  );
+}
+
+export default ChatLoadingSpinner;
