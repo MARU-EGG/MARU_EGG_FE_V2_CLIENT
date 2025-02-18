060f2368e50460be0396597d7d82be7769e57597
qwer0114 <rnalgh0114@gmail.com>
2025-02-18 16:58:34 +0900

    ✨ feat(custom): 수시,정시,편입 사용 가능여부 api 호출 추가

--- src/page/components/chat-step/choose-admission/choose-admission.tsx
+++ src/page/components/chat-step/choose-admission/choose-admission.tsx
@@ -1,4 +1,7 @@
 import PresetButton from '@/components/preset-button/preset-button';
+import Snackbar from '@/components/snackbar/snackbar';
+import useAdmissionStatus from '@/hooks/querys/useAdmissionStatus';
+import useAdmissionStatusMessage from '@/hooks/use-admission-status-message';
 import useAdmissionStore from '@/stores/store/admission-store';
 import useMessagesStore from '@/stores/store/message-store';
 import { ADDMISSION, AdmissionType } from '@/types/admission-type';
@@ -11,7 +14,10 @@ interface Props {
 
 function ChooseAdmission({ changeStep }: Props) {
   const admissions: AdmissionType[] = ['SUSI', 'JEONGSI', 'PYEONIP'];
+  const { data: admissionStatus } = useAdmissionStatus();
   const { setAdmissionType } = useAdmissionStore();
+  const { message, openSnackbar, closeSnackbar } = useAdmissionStatusMessage(admissionStatus);
+
   const { setMessages } = useMessagesStore();
 
   const selectAdmission = (admission: AdmissionType) => {
@@ -27,16 +33,26 @@ function ChooseAdmission({ changeStep }: Props) {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="mt-2 flex w-full flex-col items-end gap-2">
-        {admissions.map((admission) => (
-          <PresetButton
-            key={admission}
-            onClick={() => selectAdmission(admission)}
-          >{`${ADDMISSION[admission]} 전형이 궁금해요`}</PresetButton>
-        ))}
+    <>
+      <div className="flex flex-col">
+        <div className="mt-2 flex w-full flex-col items-end gap-2">
+          {admissions.map((admission) => (
+            <PresetButton
+              key={admission}
+              onClick={() => selectAdmission(admission)}
+              disabled={!admissionStatus[admission]}
+            >{`${ADDMISSION[admission]} 전형이 궁금해요`}</PresetButton>
+          ))}
+        </div>
       </div>
-    </div>
+      <Snackbar
+        open={openSnackbar}
+        handleClose={closeSnackbar}
+        message={message}
+        position="top"
+        autoHideDuration={3000}
+      />
+    </>
   );
 }
 
