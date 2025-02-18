9d30cc15eadff9fe6d7c5f50d1e80c55a8a2e3c0
qwer0114 <rnalgh0114@gmail.com>
2025-02-18 23:09:12 +0900

    💄 style(custom): 스타일 변경

--- src/components/loading/chat-loading-spinner.tsx
+++ src/components/loading/chat-loading-spinner.tsx
@@ -1,12 +1,16 @@
 import ChatLoadingAnimation from '@/assets/lottie/chat-loading.json';
-import Chat from '@/components/chat/chat';
+import { MaruIcon } from '@/assets/svg';
 import Lottie from 'lottie-react';
 
 function ChatLoadingSpinner() {
   return (
-    <Chat role="system">
-      <Lottie animationData={ChatLoadingAnimation} data="채팅"></Lottie>
-    </Chat>
+    <div className="relative flex">
+      <MaruIcon className="absolute -left-9 top-6" />
+      <div className="flex flex-col gap-1">
+        <div className="px-3 text-xs text-gray-500">마루에그</div>
+        <Lottie animationData={ChatLoadingAnimation} data="채팅" />
+      </div>
+    </div>
   );
 }
 
