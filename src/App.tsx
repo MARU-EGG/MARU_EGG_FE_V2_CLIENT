7d3a77272268ae8027770cd82843f58c282c8dd2
Choi JunHo <duekzim@gmail.com>
2025-07-21 11:40:53 +0900

    📦 chore(custom): 잘못된 import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga4';
 import { Route, Routes } from 'react-router-dom';
 import MaruEgg from '@/page/maru-egg';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   useEffect(() => {
